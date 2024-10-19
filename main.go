a712b7906df55036d5acce9d168862e0eb44a6c4
Raezil <kamil.mazurek319@gmail.com>
2024-10-19 14:14:29 +0200

    feat: adding authUnaryInterceptor

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"backend"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 )
 
 type Server struct {
@@ -27,6 +28,27 @@ func (s *Server) SayGoodbye(ctx context.Context, req *backend.GoodbyeRequest) (*
 	}, nil
 }
 
+func authUnaryInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	if info.FullMethod == "/authenticator.Auth/Login" || info.FullMethod == "/authenticator.Auth/Register" {
+		return handler(ctx, req)
+	}
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return nil, fmt.Errorf("missing metadata")
+	}
+	token := md["authorization"]
+	if len(token) == 0 {
+		return nil, fmt.Errorf("missing token")
+	}
+
+	claims, err := backend.VerifyJWT(token[0])
+	ctx = metadata.AppendToOutgoingContext(ctx, "current_user", claims.Email)
+	if err != nil {
+		return nil, fmt.Errorf("unauthorized: %v", err)
+	}
+	return handler(ctx, req)
+}
+
 func main() {
 	conn, err := net.Listen("tcp", ":8080")
 	if err != nil {
@@ -34,7 +56,9 @@ func main() {
 	}
 	defer conn.Close()
 
-	server := grpc.NewServer()
+	server := grpc.NewServer(
+		grpc.UnaryInterceptor(authUnaryInterceptor),
+	)
 	backend.RegisterGreeterServer(server, &Server{})
 	server.Serve(conn)
 }
