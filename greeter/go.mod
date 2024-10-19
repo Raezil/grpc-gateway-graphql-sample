a470b76b23f40b2a8fa937c910e805d26568be85
Raezil <kamil.mazurek319@gmail.com>
2024-10-19 14:01:29 +0200

    feat: initialize app

---
+++ greeter/go.mod
@@ -0,0 +1,19 @@
+module greeter
+
+go 1.22.2
+
+require (
+	github.com/graphql-go/graphql v0.8.1
+	github.com/pkg/errors v0.9.1
+	github.com/ysugimoto/grpc-graphql-gateway v0.23.0
+	google.golang.org/grpc v1.67.1
+	google.golang.org/protobuf v1.35.1
+)
+
+require (
+	github.com/iancoleman/strcase v0.0.0-20191112232945-16388991a334 // indirect
+	golang.org/x/net v0.30.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20241015192408-796eee8c2d53 // indirect
+)
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"greeter"
+
+	"google.golang.org/grpc"
+)
+
+type Server struct {
+	greeter.UnimplementedGreeterServer
+}
+
+func (s *Server) SayHello(ctx context.Context, req *greeter.HelloRequest) (*greeter.HelloReply, error) {
+	return &greeter.HelloReply{
+		Message: fmt.Sprintf("Hello, %s!", req.GetName()),
+	}, nil
+}
+
+func (s *Server) SayGoodbye(ctx context.Context, req *greeter.GoodbyeRequest) (*greeter.GoodbyeReply, error) {
+	return &greeter.GoodbyeReply{
+		Message: fmt.Sprintf("Good-bye, %s!", req.GetName()),
+	}, nil
+}
+
+func main() {
+	conn, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	server := grpc.NewServer()
+	greeter.RegisterGreeterServer(server, &Server{})
+	server.Serve(conn)
+}
